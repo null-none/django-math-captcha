48b4f2d030ac98254c604f42ddd0151544d32f2b
Dmitry Kalinin <kalinin.mitko@gmail.com>
2022-09-04 22:17:52 +0300

    Initial project

---
+++ README.md
@@ -0,0 +1,16 @@
+django-math-captcha
+=================
+
+Simple match captacha
+
+
+Installation
+------------
+
+    pip install django-math-captacha
+
+Add app in your ``settings.py``
+
+    INSTALLED_APPS = [
+        "math_captcha",
+    ]
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="django-math-captcha",
+    version="1.0.0",
+    description="Simple match captacha",
+    url="https://github.com/null-none/django-math-captcha",
+    author_email="kalinin.mitko@gmail.com",
+)
